c6ab6d8e6daf2a7999ed06b043d7168f0825a2e5
Jack Doyle <147415482+jackdoylejackdoyle@users.noreply.github.com>
2023-11-29 22:17:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -18,6 +18,17 @@ import pickle
 
 st.set_page_config(page_title="Heart Attack Analysis App")
 
+css_url = "https://github.com/jackdoylejackdoyle/ml_flow-FINAL-1/blob/main/custom.css"
+
+st.markdown(
+    f"""
+    <style>
+        {open(css_url).read()}
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 # Set the title of the web app
 st.title("Heart Attack Analysis")
 
