758a215708e0d9cbf3af59ab72eca6729c600d23
Jack Doyle <147415482+jackdoylejackdoyle@users.noreply.github.com>
2023-12-05 15:42:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -98,7 +98,7 @@ if app_mode == "Prediction":
     X = df.drop(labels="Heart Attack Prediction", axis=1)
     y = df["Heart Attack Prediction"]
     X_train, X_test, y_train, y_test = train_test_split(X,y,test_size = 0.7)
-  model_mode = st.sidebar.selectbox("Select Model",["LinearRegression", "DecisionTreeClassifier", "LogisticRegression"])
+    model_mode = st.sidebar.selectbox("Select Model",["LinearRegression", "DecisionTreeClassifier", "LogisticRegression"])
     lm = LinearRegression()
     lm.fit(X_train,y_train)
     predictions = lm.predict(X_test)
