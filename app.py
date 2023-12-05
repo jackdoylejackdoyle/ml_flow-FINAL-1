36937b7d293dfa077d5de8c8c8b585569e796dc5
Jack Doyle <147415482+jackdoylejackdoyle@users.noreply.github.com>
2023-12-05 17:05:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -173,8 +173,8 @@ if app_mode == 'Deployment':
     #logged_model = f'./mlruns/1/a768fe9670c94e098f3ab45564f0db8d/artifacts/top_model_v1'
     #loaded_model = mlflow.pyfunc.load_model(logged_model)
     model_mode = st.sidebar.selectbox("Select Model",["LinearRegression","DecisionTreeClassifier", "LogisticRegression"])
-    if model_mode == 'LinearRegression': 
-      model_filename ='LinearRegression2.pkl'
+    if model_mode == 'KNN': 
+      model_filename ='kneighbors.pkl'
       with open(model_filename, 'rb') as file:
         loaded_model = pickle.load(file)
 
