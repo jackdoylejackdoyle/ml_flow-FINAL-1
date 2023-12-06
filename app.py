7b1fe6d6e39a25a037770058e0a41b45f6e8eb16
Jack Doyle <147415482+jackdoylejackdoyle@users.noreply.github.com>
2023-12-06 01:12:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -266,11 +266,11 @@ if app_mode == "Analysis":
   st.bar_chart(data=df, x=symbols[0], y=symbols[1], use_container_width=True)
 
 
-  st.markdown("1. Heart disease is most correlated with maximum heart rate, slope of peak exercise ST segment, and chest pain (presence and/or type of angina).")
+  st.markdown("1. Heart disease is most correlated with the following variables: maximum heart rate, slope of peak exercise ST segment, and chest pain (presence and/or type of angina).")
   st.markdown("2. Heart disease is highest between 41 and 59 years of age.")
   st.markdown("3. Our most accurate model uses Logistic Regression while our least accurate model uses KNN.")
   st.markdown("<br><br><br>", unsafe_allow_html=True)
-  st.write("In order to improve this model, we should consult with expert cardiologists to understand the most relevant factors and indicators of heart disease. ")
+  st.write("In order to improve this model, we should consult with expert cardiologists to understand the most relevant factors and indicators of heart disease.")
 
   # Define a session state to store the button click status
 
